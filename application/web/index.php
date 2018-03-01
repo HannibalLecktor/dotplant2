3d863a4f78dd2d97ffb6edab00646914127c669b
HannibalLecktor <fans7288@gmail.com>
2018-03-01 22:29:11 +0500

    images and categories binding

--- application/web/index.php
+++ application/web/index.php
@@ -10,20 +10,22 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
-\Yii::$classMap['app\modules\shop\ShopModule'] = '@app/web/theme/module/modules/shop/ShopModule.php';
-\Yii::$classMap['app\modules\shop\helpers\BaseOrderStageHandlers'] = '@app/web/theme/module/modules/shop/helpers/BaseOrderStageHandlers.php';
-\Yii::$classMap['app\modules\shop\helpers\PriceHelper'] = '@app/web/theme/module/modules/shop/helpers/PriceHelper.php';
-\Yii::$classMap['app\modules\shop\models\PaymentType'] = '@app/web/theme/module/modules/shop/models/PaymentType.php';
-\Yii::$classMap['app\modules\shop\models\Product'] = '@app/web/theme/module/modules/shop/models/Product.php';
-\Yii::$classMap['app\modules\shop\models\Category'] = '@app/web/theme/module/modules/shop/models/Category.php';
-\Yii::$classMap['app\modules\shop\models\ConfigConfigurationModel'] = '@app/web/theme/module/modules/shop/models/ConfigConfigurationModel.php';
-\Yii::$classMap['app\modules\shop\models\Order'] = '@app/web/theme/module/modules/shop/models/Order.php';
-\Yii::$classMap['app\modules\shop\controllers\CartController'] = '@app/web/theme/module/modules/shop/controllers/CartController.php';
-\Yii::$classMap['app\modules\shop\controllers\ProductController'] = '@app/web/theme/module/modules/shop/controllers/ProductController.php';
-\Yii::$classMap['app\modules\user\models\LoginForm'] = '@app/web/theme/module/modules/user/models/LoginForm.php';
-\Yii::$classMap['app\modules\user\controllers\UserController'] = '@app/web/theme/module/modules/user/controllers/UserController.php';
-\Yii::$classMap['app\backend\assets\BackendAsset'] = '@app/web/theme/module/backend/assets/BackendFixAsset.php';
-\Yii::$classMap['app\modules\shop\controllers\BackendCategoryController'] = '@app/web/theme/module/modules/shop/controllers/BackendCategoryController.php';
-\Yii::$classMap['app\modules\shop\controllers\BackendProductController'] = '@app/web/theme/module/modules/shop/controllers/BackendProductController.php';
+\Yii::$classMap = \yii\helpers\ArrayHelper::merge(Yii::$classMap, [
+    'app\modules\shop\ShopModule' => '@app/web/theme/module/modules/shop/ShopModule.php',
+    'app\modules\shop\helpers\BaseOrderStageHandlers' => '@app/web/theme/module/modules/shop/helpers/BaseOrderStageHandlers.php',
+    'app\modules\shop\helpers\PriceHelper' => '@app/web/theme/module/modules/shop/helpers/PriceHelper.php',
+    'app\modules\shop\models\PaymentType' => '@app/web/theme/module/modules/shop/models/PaymentType.php',
+    'app\modules\shop\models\Product' => '@app/web/theme/module/modules/shop/models/Product.php',
+    'app\modules\shop\models\Category' => '@app/web/theme/module/modules/shop/models/Category.php',
+    'app\modules\shop\models\ConfigConfigurationModel' => '@app/web/theme/module/modules/shop/models/ConfigConfigurationModel.php',
+    'app\modules\shop\models\Order' => '@app/web/theme/module/modules/shop/models/Order.php',
+    'app\modules\shop\controllers\CartController' => '@app/web/theme/module/modules/shop/controllers/CartController.php',
+    'app\modules\shop\controllers\ProductController' => '@app/web/theme/module/modules/shop/controllers/ProductController.php',
+    'app\modules\user\models\LoginForm' => '@app/web/theme/module/modules/user/models/LoginForm.php',
+    'app\modules\user\controllers\UserController' => '@app/web/theme/module/modules/user/controllers/UserController.php',
+    'app\backend\assets\BackendAsset' => '@app/web/theme/module/backend/assets/BackendFixAsset.php',
+    'app\modules\shop\controllers\BackendCategoryController' => '@app/web/theme/module/modules/shop/controllers/BackendCategoryController.php',
+    'app\modules\shop\controllers\BackendProductController' => '@app/web/theme/module/modules/shop/controllers/BackendProductController.php',
+]);
 
 (new yii\web\Application($config))->run();
