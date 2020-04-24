0f6bddee7e85f82b63865610eab53b4f926f4b6b
sotavant <fans7288@gmail.com>
2020-04-24 13:21:08 +0500

    tg-21: fix for php7.4

--- application/extensions/DefaultTheme/widgets/PagesList/views/_config.php
+++ application/extensions/DefaultTheme/widgets/PagesList/views/_config.php
@@ -2,9 +2,9 @@
 /** @var null|\yii\base\Model $configurationModel */
 /** @var \app\extensions\DefaultTheme\models\ThemeWidgets $widget */
 /** @var boolean $isAjax */
-/** @var \yii\web\View $this */
 /** @var \app\extensions\DefaultTheme\models\ThemeActiveWidgets $model */
-/** @var \kartik\widgets\ActiveForm $form */
+/** @var \yii\web\View $this */
+/** @var \kartik\form\ActiveForm $form */
 
 ?>
 <?= $form->field(
